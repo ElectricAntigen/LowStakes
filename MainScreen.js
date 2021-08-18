f59d60d86cc2362798c6db080858c73b8d95c5f4
ElectricAntigen <82178869+ElectricAntigen@users.noreply.github.com>
2021-08-18 18:08:29 +0100

    Add files via upload

---
+++ MainScreen.js
@@ -0,0 +1,95 @@
+import React from "react";
+import {
+  Box,
+  Image,
+  Text,
+  Button,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  Stack,
+  VStack,
+  Code,
+  Flex,
+  View,
+  Spacer,
+  FlatList,
+} from "native-base";
+
+const StockLine = (props) => (
+  <HStack flex={1} alignItems="center" w="100%">
+    <Box
+      space={20}
+      w="100%"
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-between"
+      border={1}
+      borderColor="lightgray"
+      paddingLeft={2}
+      paddingRight={2}
+      bg={props.bg}
+    >
+      <Box w="50%" ph={10} alignItems="flex-start" justifyContent="center">
+        <Heading paddingLeft={5}>{props.symbol}</Heading>
+      </Box>
+      <Box
+        w="15%"
+        alignItems="flex-end"
+        justifyContent="center"
+        flex={1}
+        paddingRight={5}
+        my={2}
+      >
+        <Text>{props.quantity}</Text>
+      </Box>
+      <Box
+        bg={props.good ? "rgb(215,0,0)" : "rgb(0,215,0)"}
+        size={16}
+        rounded="md"
+        alignItems="flex-end"
+        justifyContent="center"
+        flex={1}
+        paddingRight={5}
+        my={2}
+      >
+        <Text color="white">{props.price}</Text>
+      </Box>
+    </Box>
+  </HStack>
+);
+
+export default function MainScreen() {
+  const data = [
+    { symbol: "AMAZ", price: 12.7, quantity: 12, good: true },
+    { symbol: "AMD", price: 22.5, quantity: 25, good: false },
+    { symbol: "AAPL", price: 66.9, quantity: 11, good: true },
+    { symbol: "BBC", price: 13.2, quantity: 23, good: false },
+    { symbol: "BTC", price: 98.1, quantity: 55, good: true },
+  ];
+  return (
+    <NativeBaseProvider>
+      <FlatList
+        paddingTop={20}
+        data={data.map((item, index) => {
+          return { ...item, index };
+        })}
+        renderItem={({ item }) => (
+          <StockLine
+            symbol={item.symbol}
+            price={item.price}
+            good={item.good}
+            quantity={item.quantity}
+            bg={item.index % 2 === 0 ? "white" : "lightgrey"}
+          />
+        )}
+        keyExtractor={(item) => item.symbol}
+      />
+    </NativeBaseProvider>
+  );
+}
---
+++ SignUp.js
@@ -0,0 +1,110 @@
+import * as React from "react";
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
+import {
+  NativeBaseProvider,
+  Box,
+  Text,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Link,
+  Button,
+  Icon,
+  IconButton,
+  HStack,
+  Divider,
+} from "native-base";
+
+export default function SignUp(props) {
+  return (
+    <Box safeArea flex={1} p={2} w="90%" mx="auto">
+      <Heading size="lg" color="primary.500">
+        Welcome
+      </Heading>
+      <Heading color="muted.400" size="xs">
+        Sign up to continue!
+      </Heading>
+
+      <VStack space={2} mt={5}>
+        <FormControl>
+          <FormControl.Label
+            _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
+          >
+            Email
+          </FormControl.Label>
+          <Input />
+        </FormControl>
+        <FormControl>
+          <FormControl.Label
+            _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
+          >
+            Password
+          </FormControl.Label>
+          <Input type="password" />
+        </FormControl>
+        <FormControl>
+          <FormControl.Label
+            _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
+          >
+            Confirm Password
+          </FormControl.Label>
+          <Input type="password" />
+        </FormControl>
+        <VStack space={2} mt={5}>
+          <Button
+            colorScheme="cyan"
+            _text={{ color: "white" }}
+            onPress={() => props.openPage("mainScreen")}
+          >
+            Sign Up
+          </Button>
+
+          <HStack justifyContent="center" alignItem="center">
+            <IconButton
+              variant="unstyled"
+              startIcon={
+                <Icon
+                  as={<MaterialCommunityIcons name="facebook" />}
+                  color="muted.700"
+                  size="sm"
+                />
+              }
+            />
+            <IconButton
+              variant="unstyled"
+              startIcon={
+                <Icon
+                  as={<MaterialCommunityIcons name="google" />}
+                  color="muted.700"
+                  size="sm"
+                />
+              }
+            />
+            <IconButton
+              variant="unstyled"
+              startIcon={
+                <Icon
+                  as={<MaterialCommunityIcons name="github" />}
+                  color="muted.700"
+                  size="sm"
+                />
+              }
+            />
+          </HStack>
+        </VStack>
+        <HStack justifyContent="center">
+          <Text fontSize="sm" color="muted.700" fontWeight={400}>
+            I already have an account.{" "}
+          </Text>
+          <Link
+            _text={{ color: "cyan.500", bold: true, fontSize: "sm" }}
+            onPress={() => props.openPage("login")}
+          >
+            Login
+          </Link>
+        </HStack>
+      </VStack>
+    </Box>
+  );
+}
